0b3df3708d98cef4c498217d34a71f34fdf59088
shir-s <shir.seroussi@mail.huji.ac.il>
2025-04-18 22:14:03 +0300

    Implemented room transition system with doors and teleport points between rooms.

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public Transform targetPosition;
+
+    private bool playerInside = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!playerInside && other.CompareTag("Player"))
+        {
+            other.transform.position = targetPosition.position;
+            playerInside = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+}
