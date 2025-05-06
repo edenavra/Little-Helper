fa1f869b552430e721bad1b91506969a9bb4cc43
rami hubeishi <rami.hubeishi@mail.huji.ac.il>
2025-05-06 20:45:06 +0300

    added dynamic layer ordering script

---
+++ Assets/Scripts/Utils/DynamicLayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Managers;
+using UnityEngine;
+
+namespace Utils
+{
+    public class DynamicLayer : MonoBehaviour
+    {
+        private SpriteRenderer _sprite; 
+        private Transform _playerTransform;
+
+        private void Awake()
+        {
+            _playerTransform = GameManager.Instance.playerObject.transform;
+            _sprite = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            if (_playerTransform.position.y > transform.position.y)
+            {
+                if (_sprite.sortingOrder != 100)
+                {
+                    _sprite.sortingOrder = 100;
+                }
+            }
+            else if (_sprite.sortingOrder != 0)
+            {
+                _sprite.sortingOrder = 0;
+            }
+        }
+    }
+}
